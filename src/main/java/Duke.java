bc4e8065c07922e3faedc5d3dfb78d3ec7ce9215
yueyuu <yueyu623@gmail.com>
2019-08-24 12:58:44 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -7,19 +8,32 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo); //start up logo
 
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?"); //greeting
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?"); //initial greeting
 
-        String userinput;
-        Scanner in = new Scanner(System.in);
+        String userinput; //what the user types in
+        Scanner in = new Scanner(System.in); //setting up to read in input from user
+        ArrayList<String> userlist = new ArrayList<String>(); //array to store userinputs: ArrayList is similar to vectors in c++
         while (true) {
-            userinput = in.nextLine();
+            userinput = in.nextLine(); // read in input
             if (userinput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
             }
-            System.out.println(userinput);
+            if (userinput.equals("list")) {
+                if (userlist.isEmpty()) {
+                    System.out.println(" ");
+                } else {
+                    for (int i = 0; i < userlist.size(); i ++) {
+                        System.out.println(i+1 + ". " + userlist.get(i));
+                    }
+                }
+                continue;
+            }
+            userlist.add(userinput); //add input into the list
+            System.out.println("added: " + userinput);
+
         }
     }
 }
