ca3fd39004e796c1be4bf8948df5a0e296a8c593
yueyuu <yueyu623@gmail.com>
2019-08-24 15:07:53 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,12 +9,33 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo); //start up logo
-
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?"); //initial greeting
+        //END OF INITIAL STARTUP
+
+
+        class Task {
+            protected String description;
+            protected boolean isDone;
+
+            public Task(String description) {
+                this.description = description;
+                this.isDone = false;
+            }
+
+            public String getStatusIcon() {
+                return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+            }
+
+            public void markAsDone() {
+                this.isDone = true;
+            }
+        }
+
+
 
         String userinput; //what the user types in
         Scanner in = new Scanner(System.in); //setting up to read in input from user
-        ArrayList<String> userlist = new ArrayList<String>(); //array to store userinputs: ArrayList is similar to vectors in c++
+        ArrayList<Task> userlist = new ArrayList<Task>(); //array to store userinputs: ArrayList is similar to vectors in c++
         while (true) {
             userinput = in.nextLine(); // read in input
             if (userinput.equals("bye")) {
@@ -25,14 +46,25 @@ public class Duke {
                 if (userlist.isEmpty()) {
                     System.out.println(" ");
                 } else {
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < userlist.size(); i ++) {
-                        System.out.println(i+1 + ". " + userlist.get(i));
+                        System.out.print(i+1 + ".[" + userlist.get(i).getStatusIcon() + "] ");
+                        System.out.println(userlist.get(i).description);
                     }
                 }
                 continue;
             }
-            userlist.add(userinput); //add input into the list
-            System.out.println("added: " + userinput);
+            if (userinput.startsWith("done ")) {
+                String completedtask = userinput.substring(5);
+                int tasknum = Integer.parseInt(completedtask);
+                userlist.get(tasknum-1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(" [" + "\u2713" + "] " + userlist.get(tasknum-1).description);
+                continue;
+            }
+            Task t = new Task(userinput);
+            userlist.add(t); //add input into the list
+            System.out.println("added: " + t.description);
 
         }
     }
