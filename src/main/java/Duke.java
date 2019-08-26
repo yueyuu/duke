f08dcf8be5433af6935ff7c8c33b9a5e7e3aed27
yueyuu <yueyu623@gmail.com>
2019-08-27 00:06:35 +0800

    Level 7 Save (output for event wrong)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,111 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
+
+    //TASK CLASS
+    static class Task { //same as ToDos
+        protected String description;
+        protected boolean isDone;
+        protected String identifier;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+            this.identifier = "[T]";
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        }
+        public void markAsDone() {
+            this.isDone = true;
+        }
+        public String format() {
+            return (this.identifier + "[" + this.getStatusIcon() + "] " + this.description);
+        }
+    }
+
+    static class Deadline extends Task {
+        protected String dueDate; //includes date and time
+
+        public Deadline (String description, String dueDate) {
+            super(description);
+            this.dueDate = dueDate;
+            this.identifier = "[D]";
+        }
+
+        @Override
+        public String format() {
+            return (super.format() + " (by: " + this.dueDate + ")");
+        }
+    }
+
+    static class Event extends Deadline {
+        public Event(String description, String period) {
+            super(description, period);
+            this.identifier = "[E]";
+        }
+
+        @Override
+        public String format() {
+            return (super.format() + " (at: " + this.dueDate + ")");
+        }
+    }
+
+    public static void writeToFile (Task task) throws IOException {
+        FileWriter file = new FileWriter("src/main/java/Data.txt", true); //curr working dir is duke
+        file.write(task.identifier + ';');
+        file.write(Boolean.toString(task.isDone) + ';');
+        file.write(task.description + ';');
+        if (!task.identifier.equals("[T]")) {
+            file.write(((Deadline)task).dueDate + ';');
+        }
+        file.close();
+    }
+
+    public static void overrideFile (ArrayList<Task> list) throws IOException {
+        FileWriter file = new FileWriter("src/main/java/Data.txt");
+        file.write(""); // to clear the file
+        file.close();
+        for (Task l : list) {
+            writeToFile(l);
+        }
+    }
+
+    public static void readFromFile (ArrayList<Task> list) throws FileNotFoundException {
+        File data = new File("src/main/java/Data.txt");
+        Scanner s = new Scanner(data);
+        s.useDelimiter(";");
+        while (s.hasNext()) {
+            String id = s.next();
+            String done = s.next();
+            String des = s.next();
+            Task t;
+            if (id.equals("[D]")) {
+                String due = s.next();
+                t = new Deadline(des, due);
+                if (done.equals("true")) {
+                    t.markAsDone();
+                }
+            } else if (id.equals("[E]")) {
+                String due = s.next();
+                t = new Event(des, due);
+            } else {
+                t = new Task(des);
+            }
+            if (done.equals("true")) {
+                t.markAsDone();
+            }
+            list.add(t);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -12,67 +115,15 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?"); //initial greeting
         //END OF INITIAL STARTUP
 
-        //TASK CLASS
-        class Task { //same as ToDos
-            protected String description;
-            protected boolean isDone;
-            protected String identifier;
-
-            public Task(String description) {
-                this.description = description;
-                this.isDone = false;
-                this.identifier = "[T]";
-            }
-
-            public String getStatusIcon() {
-                return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
-            }
-            public void markAsDone() {
-                this.isDone = true;
-            }
-            public String format() {
-                return (this.identifier + "[" + this.getStatusIcon() + "] " + this.description);
-            }
-        }
-
-        class Deadline extends Task {
-            protected String dueDate; //includes date and time
-
-            public Deadline (String description, String dueDate) {
-                super(description);
-                this.dueDate = dueDate;
-                this.identifier = "[D]";
-            }
-
-            @Override
-            public String format() {
-                return (super.format() + " (by: " + this.dueDate + ")");
-            }
-        }
-
-        class Event extends Task {
-            protected String period; //includes date and timing of event
-
-            public Event(String description, String period) {
-                super(description);
-                this.period = period;
-                this.identifier = "[E]";
-            }
-
-            @Override
-            public String format() {
-                return (super.format() + " (at: " + this.period + ")");
-            }
-        }
-
-
         String userinput; //what the user types in
         Scanner in = new Scanner(System.in); //setting up to read in input from user
         ArrayList<Task> userlist = new ArrayList<Task>(); //array to store userinputs: ArrayList is similar to vectors in c++
+        readFromFile(userlist);
         Task t;
 
+
         while (true) {
-            userinput = in.nextLine(); // read in input
+            userinput = in.nextLine(); // read in input (thw whole line)
             //BYE
             if (userinput.equals("bye") || userinput.equals("bye ")) {
                 System.out.println("Bye. Hope to see you again soon!");
@@ -100,6 +151,7 @@ public class Duke {
                             userlist.get(tasknum - 1).markAsDone();
                             System.out.println("Nice! I've marked this task as done:");
                             System.out.println(" [" + "\u2713" + "] " + userlist.get(tasknum - 1).description);
+                            overrideFile(userlist);
                         } else {
                             System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                             t = new Task(null);
@@ -152,7 +204,8 @@ public class Duke {
                 }
             }
 
-            userlist.add(t); //add input into the list
+            userlist.add(t); //add input into the arraylist
+            writeToFile(t);
             System.out.println("Got it. I've added this task: ");
             System.out.println(" " + t.format());
             System.out.printf("Now you have %d tasks in the list.\n", userlist.size());
