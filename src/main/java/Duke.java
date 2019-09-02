cd3241ea126a1bdac3f68bb0a3220a1d7538f0f0
yueyuu <yueyu623@gmail.com>
2019-09-02 18:43:09 +0800

    A-MoreOOP: edited Parser class
    put most of the rest of the code in main into a method in Parser

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,64 +11,12 @@ public class Duke {
 
 
         while (true) {
-            String[] userInput = parser.readInput();
-
-            if (userInput[0].equals("")) { //user enters nothing
-                Ui.printEnterSomething();
-            } else if (userInput[0].equals("bye")) { //BYE
-                Ui.printBye();
-                return;
-            } else if (userInput[0].equals("list")) { //LIST
-                Ui.printList(list.userList.isEmpty(), list.userList);
-            } else  {
-                try {
-                    if (userInput[0].equals("done")) { //DONE
-                        try {
-                            int taskNum = Integer.parseInt(userInput[1]) - 1; //if fail gives exception n
-                            list.doneTask(taskNum);
-                            Ui.printDone(list.userList, taskNum);
-                        } catch (NumberFormatException n) {
-                            System.out.println("The task number has to be in digits. :)");
-                        }
-                    } else if (userInput[0].equals("delete")) { //REMOVING TASK
-                        try {
-                            int taskNum = Integer.parseInt(userInput[1])-1;
-                            Ui.printDelete(list.userList, taskNum);
-                            list.deleteTask(taskNum);
-                        } catch (NumberFormatException f) {
-                            System.out.println("The task number has to be in digits. :)");
-                        }
-                    } else if (userInput[0].equals("find")) { //FINDING TASK: search for keyword
-                        String keyword = userInput[1];
-                        ArrayList<Task> match = new ArrayList<Task>(); //to store task that contains the keyword
-                        list.findTask(match, keyword);
-                        Ui.printFind(match);
-                    } else if (userInput[0].equals("todo")) {
-                        Task t = new Task(userInput[1]);
-                        list.addTask(t);
-                    } else if (userInput[0].equals("deadline")) {
-                        String[] deadline = userInput[1].split(" /by ", 2);
-                        try {
-                            Task t = new Deadline(deadline[0], deadline[1]);
-                            list.addTask(t);
-                        }  catch (DateTimeParseException | ArrayIndexOutOfBoundsException e) {
-                            Ui.printDeadlineError();
-                        }
-                    } else if (userInput[0].equals("event")) {
-                        String[] event = userInput[1].split(" /at ", 2);
-                        try {
-                            Task t = new Event(event[0], event[1]);
-                            list.addTask(t);
-                        } catch (DateTimeParseException | ArrayIndexOutOfBoundsException ex) {
-                            Ui.printEventError();
-                        }
-                    } else {
-                        Ui.printDono();
-                    }
+            parser.readInput();
+            try {
+                parser.parse(list);
                 } catch (ArrayIndexOutOfBoundsException e) {
-                    Ui.printEmptyDescription(userInput[0]);
+                    Ui.printEmptyDescription(parser.command);
                 }
             }
         }
-    }
 }
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,11 @@
+import java.io.IOException;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Parser {
     //deals with making sense of the user command
+    protected String[] userInput;
     protected String command;
     protected Scanner in;
 
@@ -9,11 +13,62 @@ public class Parser {
         this.in = new Scanner(System.in); //setting up to read in input from user
     }
 
-    public String[] readInput() {
+    public void readInput() {
         String input = in.nextLine().trim(); // read in input (the whole line)
-        return input.split(" ", 2); //extract out the command word
+        userInput =  input.split(" ", 2); //extract out the command word
+        command = userInput[0];
     }
-    public void parse() {
 
+    public void parse(TaskList list) throws IOException {
+        if (command.equals("")) { //user enters nothing
+            Ui.printEnterSomething();
+        } else if (command.equals("bye")) { //BYE
+            Ui.printBye();
+            System.exit(0);
+        } else if (command.equals("list")) { //LIST
+            Ui.printList(list.userList.isEmpty(), list.userList);
+        } else if (command.equals("done")) { //DONE
+            try {
+                int taskNum = Integer.parseInt(userInput[1]) - 1; //if fail gives exception n
+                list.doneTask(taskNum);
+                Ui.printDone(list.userList, taskNum);
+            } catch (NumberFormatException | IOException n) {
+                System.out.println("The task number has to be in digits. :)");
+            }
+        } else if (userInput[0].equals("delete")) { //REMOVING TASK
+            try {
+                int taskNum = Integer.parseInt(userInput[1])-1;
+                Ui.printDelete(list.userList, taskNum);
+                list.deleteTask(taskNum);
+            } catch (NumberFormatException | IOException f) {
+                System.out.println("The task number has to be in digits. :)");
+            }
+        } else if (userInput[0].equals("find")) { //FINDING TASK: search for keyword
+            String keyword = userInput[1];
+            ArrayList<Task> match = new ArrayList<Task>(); //to store task that contains the keyword
+            list.findTask(match, keyword);
+            Ui.printFind(match);
+        } else if (userInput[0].equals("todo")) {
+            Task t = new Task(userInput[1]);
+            list.addTask(t);
+        } else if (userInput[0].equals("deadline")) {
+            String[] deadline = userInput[1].split(" /by ", 2);
+            try {
+                Task t = new Deadline(deadline[0], deadline[1]);
+                list.addTask(t);
+            }  catch (DateTimeParseException | ArrayIndexOutOfBoundsException e) {
+                Ui.printDeadlineError();
+            }
+        } else if (userInput[0].equals("event")) {
+            String[] event = userInput[1].split(" /at ", 2);
+            try {
+                Task t = new Event(event[0], event[1]);
+                list.addTask(t);
+            } catch (DateTimeParseException | ArrayIndexOutOfBoundsException ex) {
+                Ui.printEventError();
+            }
+        } else {
+            Ui.printDono();
+        }
     }
 }
