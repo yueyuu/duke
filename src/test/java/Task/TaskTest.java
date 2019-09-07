e36e38bd33d1810cb88fae176b29004faa055566
yueyuu <yueyu623@gmail.com>
2019-09-07 13:42:39 +0800

    A-JUnit: Task class

---
+++ src/test/java/Task/TaskTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Task_Test")
+class TaskTest {
+    @Test
+    void testFormat() {
+        Task t = new Task("borrow book");
+        assertEquals("[T][✘] borrow book", t.format());
+        t.markAsDone();
+        assertEquals("[T][✓] borrow book", t.format());
+    }
+}
